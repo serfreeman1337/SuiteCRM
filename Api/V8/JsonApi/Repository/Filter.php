b8e2a816850aae409095cd8edcc2e59b48319725
serfreeman1337 <serfreeman1337@yandex.com>
2020-01-27 23:34:32 +0300

    Fixed #8366 - V8 API Filtering W/ OR Operator Chained Conditions

--- Api/V8/JsonApi/Repository/Filter.php
+++ Api/V8/JsonApi/Repository/Filter.php
@@ -43,7 +43,14 @@ class Filter
             unset($params['operator']);
         }
 
-        $params = $this->addDeletedParameter($params);
+        $deleted = false;
+        if (isset($params['deleted'])) {
+            if (isset($params['deleted']['eq'])) {
+                $deleted = ($params['deleted']['eq'] == 1);
+            }
+            
+            unset($params['deleted']);
+        }
 
         $where = [];
         foreach ($params as $field => $expr) {
@@ -71,24 +78,20 @@ class Filter
             }
         }
 
-        return implode(sprintf(' %s ', $operator), $where);
-    }
-
-    /**
-     * Only return deleted records if they were explicitly requested
-     *
-     * @param array $params
-     * @return array
-     */
-    protected function addDeletedParameter(array $params)
-    {
-        if (!array_key_exists('deleted', $params)) {
-            $params['deleted'] = [
-                'eq' => 0
-            ];
+        if (empty($where)) {
+            return sprintf(
+                "%s.deleted = '%d'",
+                $bean->getTableName(),
+                $deleted
+            );
         }
 
-        return $params;
+        return sprintf(
+            "(%s) AND %s.deleted = '%d'",
+            implode(sprintf(' %s ', $operator), $where),
+            $bean->getTableName(),
+            $deleted
+        );
     }
 
     /**
